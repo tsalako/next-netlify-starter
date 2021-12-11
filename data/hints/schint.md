0238562dde430fcfb4d661d433757dd4d369c1bb
Oluwatofe <tsalako12@gmail.com>
2021-12-10 21:58:27 -0800

    updating hint
    ayo

--- data/hints/schint.md
+++ data/hints/schint.md
@@ -1,7 +1,8 @@
 ---
-question: 'A neighborhood spot could be the stop<br/>
-That you at present seek<br/><br/>
-So open a book and take a look<br/>
-Mayhaps you’ll find yourself a drink<br/>'
+question: 'Would it be the holidays without a festive libation?<br/>
+Perhaps something quick and effective, a potent creation.<br/><br/>
+
+A place whose name you’d find in a book, but don’t seek the words in ink<br/>
+It’s what makes a book, that’s where to look, when searching for a drink.<br/>'
 answer: Santa Claus
 ---
--- pages/congrats.js
+++ pages/congrats.js
@@ -13,10 +13,10 @@ export default function FinalHint() {
       </Head>
       <main>
         <Banner />
-        <h2>My my, it seems like you're quite the sleuth<br/>But we have one last directive for you</h2>
+        <h2>My my, you're clearly quite the sleuth<br/>But we have one last directive for you</h2>
         <br/>
         <div class="box">
-          Quick! Santa is delivering presents to The Tanners this Christmas.<br/>If you hurry you’ll find your yuletide treasure.
+          Quick! Santa is delivering presents to The Tanners.<br/>So hurry, run, push and shove, no need to mind your manners!
         </div>
       </main>
       <Footer />
